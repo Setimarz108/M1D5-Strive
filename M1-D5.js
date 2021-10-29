ec0bb7e23a2f867ed33e761a6617e4fa861f63e6
Setimarz108 <66173614+Setimarz108@users.noreply.github.com>
2021-10-29 15:17:31 +0200

    finished Ex. 2
    crazySum

--- M1-D5.js
+++ M1-D5.js
@@ -1,78 +1,92 @@
-// Additional assignments for Day 5
+/*/ASSIGNMENT RULES
+- All the answers must be written in JavaScript
+- You can ask for help, reach the Teaching Assistants if needed
+- You can Google / use StackOverflow BUT only when you think you need something we didn't cover during lessons yet
+- You can test your code in a separate file or de-commenting the single exercises in this one.
+- You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
+- The solution must be pushed to the repository and be available for the tutors by the end of the day (5PM CET)
+*/
 
-/* EXTRA 1
- Write a function called "checkArray" which receives an array of random numbers (created with giveMeRandom) and prints, for each item, whether it's bigger than 5 or not.
- The function should return the sum of just the numbers bigger than 5.
+/* EXERCISE 1
+ Write a function called "area" which receives 2 parameters (l1, l2) and calculates the area of the associated rectangle.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+function area (width, hight){
+    
+    let area = width * hight
+   return area
+}
+area(12,12)
+
+/* EXERCISE 2
+ Write a function called "crazySum" which receives two integers as parameters.
+ It should return the sum of those two values, but if the two values are the same then it should return their sum multiplied by 3.
 */
 
 /* WRITE YOUR ANSWER HERE */
 
-/* EXTRA 2
- In your eCommerce you have an array of objects called shoppingCart. Each one of these objects has a price, a name, an id and the quantity to be shipped.
- Create a function called "shoppingCartTotal" which calculates the total due to the shop.
+function crazySum(int1, int2){
+    
+    let sumForEqualNums = int1 + int2 -3
+    let sum = int1 + int2
+
+    if(int1 === int2){
+        return sumForEqualNums
+    }else {
+        return sum
+    }
+}
+
+crazySum(8,2)
+
+/* EXERCISE 3
+ Write a function called "crazyDiff" that computes the absolute difference between a given number and 19.
+ It should return triple their absolute difference if the given number is greater than 19.
 */
 
 /* WRITE YOUR ANSWER HERE */
 
-/* EXTRA 3
- In your eCommerce you have an array of objects called shoppingCart. Each one of these objects has a price, a name, an id and the quantity to be shipped.
- Create a function called "addToShoppingCart" which receives a new object, adds it to shoppingCart and returns the total number of items in the shoppingCart.
+/* EXERCISE 4
+ Write a function called "boundary" which accept an integer parameter n and returns true if n is within 20 and 100 (included) or if n it's equal to 400.
 */
 
 /* WRITE YOUR ANSWER HERE */
 
-/* EXTRA 4
- In your eCommerce you have an array of objects called shoppingCart. Each one of these objects has a price, a name, an id and the quantity to be shipped.
- Create a function called "maxShoppingCart" which receives the shoppingCart array and returns the most expensive item in it.
+/* EXERCISE 5
+ Write a function called "strivify" which accepts a string as a parameter.
+ It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
 
 /* WRITE YOUR ANSWER HERE */
 
-/* EXTRA 5
- In your eCommerce you have an array of objects called shoppingCart. Each one of these objects has a price, a name, an id and the quantity to be shipped.
- Create a function called "latestShoppingCart" which receives the shoppingCart array and returns the last item.
+/* EXERCISE 6
+ Write a function called "check3and7" which accepts a positive number as a parameter and checks if it is a multiple of 3 or a multiple of 7.
+ HINT: Modulus Operator
 */
 
 /* WRITE YOUR ANSWER HERE */
 
-/* EXTRA 6
- Create a function called "loopUntil" which receives an integer x between 0 and 9 as a parameter.
- The function loops and prints a random number between 0 and 9 until the random number is bigger than x for three times in a row.
+/* EXERCISE 7
+ Write a function called "reverseString" which programmatically reverses a given string (es.: Strive => evirtS).
 */
 
 /* WRITE YOUR ANSWER HERE */
 
-/* EXTRA 7
- Write a function called "average" which receives an array and returns the average numerical value. The function automatically skips non-numeric entries in the array.
+/* EXERCISE 8
+ Write a function called "upperFirst" which capitalizes the first letter of each word of a given string passed as a parameter.
 */
 
 /* WRITE YOUR ANSWER HERE */
 
-/* EXTRA 8
- Write a function called "longest" to find the longest string from a given array of strings.
+/* EXERCISE 9
+ Write a function called "cutString" which creates a new string without the first and last character of a given string passed as a parameter.
 */
 
 /* WRITE YOUR ANSWER HERE */
 
-/* EXTRA 9
- Write a function to create a very simple anti spam filter for your mailbox. The function takes a string emailContent, and returns a boolean.
- The function should return true if the emailContent string does not contain the words SPAM or SCAM.
-*/
-
-/* WRITE YOUR ANSWER HERE */
-
-/* EXTRA 10
- Write a function that receives a date as a parameter and calculates the number of days passed since the given date.
-*/
-
-/* WRITE YOUR ANSWER HERE */
-
-/* EXTRA 11
- Write a function called "matrixGenerator" which receives two integers, x and y, as parameters.
- The result should be a matrix of x times y with, as value, the index of the position.
- Ex.: x = 3, y = 2
- ["00","01","02"
- "10","11","12"]
+/* EXERCISE 10
+ Write a function called "giveMeRandom" which accepts a number n and returns an array containing n random numbers between 0 and 10.
 */
 
 /* WRITE YOUR ANSWER HERE */
